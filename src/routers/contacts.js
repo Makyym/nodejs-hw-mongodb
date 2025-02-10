02aecbb8394cd61534a2a5531ee487a91203e371
Makyym <168359394+Makyym@users.noreply.github.com>
2025-02-10 11:56:45 +0200

    step-7

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,8 +4,11 @@ import { createContactController, deleteContactController, getContactByIdControl
 import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { isValidId } from "../middlewares/isValidId.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
 
